81068ea22f8bae002592adf43e58f4ae3be1fbfb
YahyaNashar22 <yahyanashar22@gmail.com>
2024-07-16 16:52:15 +0300

    seperated logic for mobile view and desktop view on projects component

--- components/Works.tsx
+++ components/Works.tsx
@@ -11,11 +11,13 @@ import { fadeIn, textVariant } from "@/utils/motion";
 import Image, { StaticImageData } from "next/image";
 import { useEffect, useState } from "react";
 import ShortPreview from "./ShortPreview";
+import ProjectsDesktopView from "./ProjectsDesktopView";
+import ProjectsMobileView from "./ProjectsMobileView";
 
 type ProjectCardProp = {
   index: number;
   name: string;
-  description: String;
+  description: string;
   tags: any[];
   image: StaticImageData;
   video?: string;
@@ -62,94 +64,35 @@ const ProjectCard: React.FC<ProjectCardProp> = ({
       }}
       onHoverEnd={() => setIsShortPreview(false)}
     >
-      <Tilt
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full relative"
-      >
-        <div className="relative w-full h-[240px]">
-          {isShortPreview && video && <ShortPreview video={video} />}
-
-          {!isShortPreview && (
-            <Image
-              src={image}
-              alt={name}
-              className="w-full h-full object-cover rounded-2xl"
-              placeholder="blur"
-            />
-          )}
-
-          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
-            {/* Backend Github Repo Link */}
-            {source_code_link_back_end && (
-              <div
-                onClick={() => window.open(source_code_link_back_end, "_blank")}
-                className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer relative z-[100]"
-              >
-                <Image
-                  src={github}
-                  alt="github_back_end"
-                  className="w-1/2 h-1/2 object-contain"
-                />
-                <span className="absolute top-0 right-0 bg-red-500 text-white rounded-full text-xs w-5 h-5 flex justify-center items-center">
-                  B
-                </span>
-              </div>
-            )}
-            {/* Frontend Github Repo Link */}
-            {source_code_link_front_end && (
-              <div
-                onClick={() =>
-                  window.open(source_code_link_front_end, "_blank")
-                }
-                className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer relative z-[100]"
-              >
-                <Image
-                  src={github}
-                  alt="github_front_end"
-                  className="w-1/2 h-1/2 object-contain"
-                />
-                <span className="absolute top-0 right-0 bg-yellow-500 text-white rounded-full text-xs w-5 h-5 flex justify-center items-center">
-                  F
-                </span>
-              </div>
-            )}
-            {/* Live Website Preview Link */}
-            {live_link && (
-              <div
-                onClick={() => window.open(live_link, "_blank")}
-                className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer relative z-[100]"
-              >
-                <Image
-                  src={live_globe}
-                  alt="live_preview"
-                  className="w-1/2 h-1/2 object-contain"
-                />
-                <span className="absolute top-0 right-0 bg-green-500 text-white rounded-full text-xs w-5 h-5 flex justify-center items-center">
-                  L
-                </span>
-              </div>
-            )}
-          </div>
-        </div>
-        <div className="mt-5">
-          <h3>
-            {name}{" "}
-            {isSmallScreen && video && (
-              <span className="float-end violet-gradient text-[14px] p-1.5 rounded-2xl">
-                <button onClick={() => setIsShortPreview(!isShortPreview)}>
-                  {isShortPreview ? "Stop preview" : "Watch preview"}
-                </button>
-              </span>
-            )}
-          </h3>
-          <p className="mt-2 text-secondary text-[14px]">{description}</p>
-        </div>
-
-      </Tilt>
+      {!isSmallScreen ? (
+        <ProjectsDesktopView
+          isShortPreview={isShortPreview}
+          video={video}
+          image={image}
+          name={name}
+          source_code_link_back_end={source_code_link_back_end}
+          github={github}
+          source_code_link_front_end={source_code_link_front_end}
+          live_link={live_link}
+          live_globe={live_globe}
+          description={description}
+          tags={tags}
+        />
+      ) : (
+        <ProjectsMobileView
+          isShortPreview={isShortPreview}
+          video={video}
+          image={image}
+          name={name}
+          source_code_link_back_end={source_code_link_back_end}
+          github={github}
+          source_code_link_front_end={source_code_link_front_end}
+          live_link={live_link}
+          live_globe={live_globe}
+          description={description}
+          tags={tags}
+        />
+      )}
     </motion.div>
   );
 };
