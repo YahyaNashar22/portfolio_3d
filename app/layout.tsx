7336485cf088434f7908700c396a2184a720aa22
YahyaNashar22 <yahyanashar22@gmail.com>
2024-07-17 08:16:21 +0300

    added proper meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { Toaster } from "react-hot-toast";
 export const metadata = {
   title: "Yahya Nashar",
   description: "Yahya Nashar, Full Stack Developer Portfolio",
+  image: "/assets/overview.png",
+  url: "https://portfolio-3d-sooty.vercel.app/",
 };
 
 export default function RootLayout({
@@ -13,6 +15,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content={metadata.image} />
+        <meta property="og:url" content={metadata.url} />
+        <meta property="og:type" content="website" />
+      </head>
       <body>
         <Toaster />
         <Navbar />
